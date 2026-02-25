81c2b152166d8deaf4cc6fd7c5e531e4766532b1
Faith <faith.wangome@strathmore.edu>
2026-02-25 09:55:25 +0300

    view changes

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 // ADD THESE TWO LINES - They link your browser to your Controller
 Route::get('/invoice/{orderId}', [InvoiceController::class, 'generateInvoice']);
 Route::get('/track/{orderId}', [InvoiceController::class, 'trackOrder']);
+Route::view('/storefront', 'storefront');
